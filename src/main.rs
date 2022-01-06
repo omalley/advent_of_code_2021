a84fa4d0948f0669f7fb02c49a29da6d3bb5544d
Owen O'Malley <omalley@apache.org>
2022-01-05 23:04:33 -0800

    puzzle 1.2

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,30 @@ fn main() {
   // Convert them to integers
   let nums: Vec<i32> = lines.iter().map(|x| x.parse::<i32>().unwrap()).collect();
   print!("Descended {}\n", count_descents(&nums));
+  print!("Triple Descended {}\n", count_triple_descents(&nums));
 }
 
 fn count_descents(nums: &Vec<i32>) -> i32 {
-  let mut result = 0;
+  let mut count = 0;
   let mut last = i32::MAX;
   for current in nums {
     if *current > last {
-      result += 1;
+      count += 1;
     }
     last = *current;
   }
-  result
+  count
+}
+
+fn count_triple_descents(nums: &Vec<i32>) -> i32 {
+  let mut count = 0;
+  let mut last = i32::MAX;
+  for i in 0..nums.len()-2 {
+    let current = nums[i] + nums[i+1] + nums[i+2];
+    if current > last {
+      count += 1;
+    }
+    last = current;
+  }
+  count
 }
