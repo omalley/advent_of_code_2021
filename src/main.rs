22c6969bdf25663beedf71d4388063265ada87fa
Owen O'Malley <omalley@apache.org>
2022-01-09 10:18:19 -0800

    problem 3.1

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,59 @@
+use std::cmp;
 use std::io;
 use std::io::BufRead;
+use std::iter;
 
 fn main() {
   // Get the input lines
   let lines: Vec<String> = io::stdin().lock().lines()
     .map(|x| String::from(x.unwrap().trim())).collect();
-  let cmds: Vec<Move> = lines.iter().map(|x| parse(x)).collect();
-  let mut posn = Position{x: 0, y: 0, aim: 0};
-  for c in cmds {
-    posn.update(&c);
-  }
-  println!("area = {}", posn.area())
+  let trans = transpose(&lines);
+  let gamma = compute_gamma(&trans);
+  let epsilon = (1 << trans.len()) - gamma - 1;
+  println!("gamma = {}, epsilon = {}, product = {}", gamma, epsilon,
+           gamma * epsilon);
 }
 
-enum Move {
-  Up(i32),
-  Down(i32),
-  Forward(i32),
-}
-
-fn parse(s: &String) -> Move {
-  let mut parts = s.split_whitespace();
-  let command = parts.next().unwrap();
-  let dist = parts.next().unwrap().parse::<i32>().unwrap();
-  match command {
-    "forward" => Move::Forward(dist),
-    "up" => Move::Up(dist),
-    "down" => Move::Down(dist),
-    _ => panic!("Unknown command {}", command),
-  }
-}
-
-struct Position {
-  x: i32,
-  y: i32,
-  aim: i32,
-}
-
-impl Position {
-  fn update(self: &mut Position, m: &Move) {
-    match m {
-      Move::Up(i) => self.aim -= i,
-      Move::Down(i) => self.aim += i,
-      Move::Forward(i) => {
-        self.x += i;
-        self.y += self.aim * i;
-      }
+// Transpose the strings so that the first character of each row beomes
+// the first row.
+fn transpose(input: &Vec<String>) -> Vec<String> {
+  let width =
+    input.iter().map(|x| x.len()).reduce(|l, r| cmp::max(l,r)).unwrap();
+  // start with width empty strings
+  let mut result: Vec<String>
+    = iter::repeat(String::new()).take(width).collect();
+  // for each line, append each character
+  for s in input {
+    let mut i = 0;
+    for ch in s.chars() {
+      result[i].push(ch);
+      i += 1;
     }
   }
+  result
+}
 
-  fn area(self: &Position) -> i32 {
-    self.x * self.y
+// compare the number of '1' versus '0' in the string
+// returns the difference
+fn compare_digits(s: &str) -> i64 {
+  let mut result = 0;
+  for c in s.chars() {
+    match c {
+      '0' => result -= 1,
+      '1' => result += 1,
+      _ => eprintln!("Unknown character {}", c),
+    }
   }
+  result
+}
+
+fn compute_gamma(s: &Vec<String>) -> i64 {
+  let mut result = 0;
+  for diff in s.iter().map(|x| compare_digits(x)) {
+    result *= 2;
+    if diff > 0 {
+      result += 1;
+    }
+  }
+  result
 }
