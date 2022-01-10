c0fa45677a914a56fb7765394bb6d65c79c3ebf8
Owen O'Malley <omalley@apache.org>
2022-01-10 12:13:05 -0800

    puzzle 6.1

--- src/main.rs
+++ src/main.rs
@@ -1,172 +1,51 @@
-use std::cmp;
 use std::io;
 use std::io::BufRead;
 
 fn main() {
   let stdin = io::stdin();
-  let lines: Vec<Line> = stdin.lock().lines()
+  let mut lines = stdin.lock().lines()
      .map(|x| String::from(x.unwrap().trim()))
-     .filter(|x| x.len() > 0)
-     .map(|x| Line::parse(x.as_str()))
-     .collect();
+     .filter(|x| x.len() > 0);
 
-  let pic = Picture::new(&lines);
+  let mut ocean = Ocean::default();
+  // parse the first line as fish ages
+  for fish in lines.next().unwrap().split(",")
+               .map(|x| x.trim().parse::<i32>().unwrap()) {
+     ocean.add(fish, 1);
+  }
+
+  for _ in 0..80 {
+    ocean.age();
+    println!("ocean = {:?}", ocean);
+  }
+  println!("total = {}", ocean.total());  
+}
+
+const BIRTH_TO_BIRTH: i32 = 9;
+const GENERATION: i32 = 7;
   
-  println!("pic = {:?}", pic);
-  println!("result = {}", pic.count(|x| x > 1));
+#[derive(Debug,Default)]
+struct Ocean {
+  count: Vec<i32>,
+  age: i32,
 }
 
-#[derive(Debug)]
-struct Point {
-  x: i64,
-  y: i64,
-}
-
-impl Point {
-  fn parse(s: &str) -> Point {
-    let mut parts = s.split(",").map(|x| x.trim().parse::<i64>().unwrap());
-    Point{x: parts.next().unwrap(), y: parts.next().unwrap()}
-  }
-}
-
-#[derive(Debug)]
-struct Line {
-  p1: Point,
-  p2: Point,
-}
-
-impl Line {
-  fn parse(s: &str) -> Line {
-    let mut parts = s.split("->").map(|x| Point::parse(x.trim()));
-    Line{p1: parts.next().unwrap(), p2: parts.next().unwrap()}
-  }
-
-  fn is_vertical(&self) -> bool {
-    self.p1.x == self.p2.x
-  }
-  
-  fn is_horizontal(&self) -> bool {
-    self.p1.y == self.p2.y
-  }
-
-  fn is_upward(&self) -> bool {
-    if self.is_vertical() || self.is_horizontal() {
-      false
-    } else if self.p2.x > self.p1.x {
-      self.p2.y < self.p1.y
-    } else {
-      self.p2.y > self.p1.y
+impl Ocean {
+  fn add(&mut self, age: i32, cnt: i32) {
+    while self.count.len() <= age as usize {
+      self.count.push(0);
     }
+    self.count[age as usize] += cnt;
   }
 
-  fn left(&self) -> i64 {
-    cmp::min(self.p1.x, self.p2.x)
+  fn age(&mut self) {
+    let children = self.count.remove(0);
+    self.add(BIRTH_TO_BIRTH - 1, children);
+    self.add(GENERATION - 1, children);
+    self.age += 1;
   }
 
-  fn right(&self) -> i64 {
-    cmp::max(self.p1.x, self.p2.x)
-  }
-
-  fn top(&self) -> i64 {
-    cmp::min(self.p1.y, self.p2.y)
-  }
-
-  fn bottom(&self) -> i64 {
-    cmp::max(self.p1.y, self.p2.y)
-  }
-}
-
-#[derive(Debug)]
-enum Bounding {
-  Empty,
-  Box{l:i64, r:i64, t:i64, b:i64},
-}
-
-impl Default for Bounding {
-  fn default() -> Self { Bounding::Empty }
-}
-
-impl Bounding {
-  fn add(&self, line: &Line) -> Bounding {
-    match self {
-      Bounding::Empty => Bounding::Box{
-        l: line.left(), r: line.right(),
-        t: line.top(), b: line.bottom()},
-      Bounding::Box{l, r, t, b} => Bounding::Box{
-        l: cmp::min(*l, line.left()),
-        r: cmp::max(*r, line.right()),
-        t: cmp::min(*t, line.top()),
-        b: cmp::max(*b, line.bottom())}
-    }
-  }
-}
-
-#[derive(Debug, Default)]
-struct Picture {
-  bounds: Bounding,
-  count: Vec<Vec<i32>>,
-}
-
-impl Picture {
-  fn new(lines: &Vec<Line>) -> Self {
-    let mut result = Picture::default();
-    result.bounds = lines.iter().fold(result.bounds, |b, l| b.add(l));
-    match result.bounds {
-      Bounding::Empty => {}
-      Bounding::Box{l, r, t, b} =>
-        for _x in l..r+1 {
-          result.count.push(Vec::new());
-          for _y in t..b+1 {
-            result.count.last_mut().unwrap().push(0);
-          }
-        }
-    }
-    for l in lines {
-      result.add(l);
-    }
-    result
-  }
-
-  fn increment(&mut self, p: &Point) {
-    match self.bounds {
-      Bounding::Empty => {},
-      Bounding::Box{l, r: _, t, b: _} =>
-        self.count[(p.x - l) as usize][(p.y - t) as usize] += 1
-    }
-  }
-  
-  fn add(&mut self, l: &Line) {
-    if l.is_horizontal() {
-      for x in l.left()..l.right()+1 {
-        self.increment(&Point{x: x, y: l.top()})
-      }
-    } else if l.is_vertical() {
-      for y in l.top()..l.bottom()+1 {
-        self.increment(&Point{x: l.left(), y: y})
-      }
-    } else if l.is_upward() {
-      for d in 0..(l.right() - l.left() + 1) {
-        self.increment(&Point{x: l.left() + d,
-                              y: l.bottom() - d});
-      }
-    } else {
-      for d in 0..(l.right() - l.left() + 1) {
-        self.increment(&Point{x: l.left() + d,
-                              y: l.top() + d});
-      }
-    }
-  }
-
-  fn count<F>(&self, f: F) -> i64
-      where F: Fn(i32) -> bool {
-    let mut result: i64 = 0;
-    for col in &self.count {
-      for loc in col {
-        if f(*loc) {
-          result += 1;
-        }
-      }
-    }
-    result
+  fn total(&self) -> i32 {
+    self.count.iter().fold(0, |acc, c| acc + c)
   }
 }
