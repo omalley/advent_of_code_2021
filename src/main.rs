ad06c070f5830b29c68c01d5b1435b030d66391d
Owen O'Malley <omalley@apache.org>
2022-01-09 23:06:49 -0800

    puzzle 5.1

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,153 @@
+use std::cmp;
 use std::io;
 use std::io::BufRead;
-use std::iter;
 
 fn main() {
   let stdin = io::stdin();
-  let mut stream = stdin.lock().lines()
+  let mut lines: Vec<Line> = stdin.lock().lines()
      .map(|x| String::from(x.unwrap().trim()))
-     .filter(|x| x.len() > 0);
+     .filter(|x| x.len() > 0)
+     .map(|x| Line::parse(x.as_str()))
+     .collect();
 
-  // read the first line as a sequence of integers
-  let moves: Vec<i32> = stream.next().unwrap().split(",")
-     .map(|x| x.trim().parse::<i32>().unwrap()).collect();
-
-  // read the rest as boards
-  let mut boards = read_board_list(stream.by_ref());
+  lines.retain(|x| x.is_vertical() || x.is_horizontal());
+  let pic = Picture::new(&lines);
   
-  println!("score = {}", run_moves(&moves, &mut boards));
+  println!("pic = {:?}", pic);
+  println!("result = {}", pic.count(|x| x > 1));
 }
 
-const BOARD_SIZE: usize = 5;
+#[derive(Debug)]
+struct Point {
+  x: i64,
+  y: i64,
+}
+
+impl Point {
+  fn parse(s: &str) -> Point {
+    let mut parts = s.split(",").map(|x| x.trim().parse::<i64>().unwrap());
+    Point{x: parts.next().unwrap(), y: parts.next().unwrap()}
+  }
+}
+
+#[derive(Debug)]
+struct Line {
+  p1: Point,
+  p2: Point,
+}
+
+impl Line {
+  fn parse(s: &str) -> Line {
+    let mut parts = s.split("->").map(|x| Point::parse(x.trim()));
+    Line{p1: parts.next().unwrap(), p2: parts.next().unwrap()}
+  }
+
+  fn is_vertical(&self) -> bool {
+    self.p1.x == self.p2.x
+  }
+  
+  fn is_horizontal(&self) -> bool {
+    self.p1.y == self.p2.y
+  }
+
+  fn left(&self) -> i64 {
+    cmp::min(self.p1.x, self.p2.x)
+  }
+
+  fn right(&self) -> i64 {
+    cmp::max(self.p1.x, self.p2.x)
+  }
+
+  fn top(&self) -> i64 {
+    cmp::min(self.p1.y, self.p2.y)
+  }
+
+  fn bottom(&self) -> i64 {
+    cmp::max(self.p1.y, self.p2.y)
+  }
+}
+
+#[derive(Debug)]
+enum Bounding {
+  Empty,
+  Box{l:i64, r:i64, t:i64, b:i64},
+}
+
+impl Default for Bounding {
+  fn default() -> Self { Bounding::Empty }
+}
+
+impl Bounding {
+  fn add(&self, line: &Line) -> Bounding {
+    match self {
+      Bounding::Empty => Bounding::Box{
+        l: line.left(), r: line.right(),
+        t: line.top(), b: line.bottom()},
+      Bounding::Box{l, r, t, b} => Bounding::Box{
+        l: cmp::min(*l, line.left()),
+        r: cmp::max(*r, line.right()),
+        t: cmp::min(*t, line.top()),
+        b: cmp::max(*b, line.bottom())}
+    }
+  }
+}
 
 #[derive(Debug, Default)]
-struct BingoBoard {
-  board: Vec<Vec<i32>>,
-  mark: [[bool; BOARD_SIZE]; BOARD_SIZE],
+struct Picture {
+  bounds: Bounding,
+  count: Vec<Vec<i32>>,
 }
 
-impl BingoBoard {
-  fn won(&self) -> bool {
-    // look for winning rows
-    for x in 0..BOARD_SIZE {
-      for y in 0..BOARD_SIZE {
-        if !self.mark[x][y] {
-          break;
+impl Picture {
+  fn new(lines: &Vec<Line>) -> Self {
+    let mut result = Picture::default();
+    result.bounds = lines.iter().fold(result.bounds, |b, l| b.add(l));
+    match result.bounds {
+      Bounding::Empty => {}
+      Bounding::Box{l, r, t, b} =>
+        for _x in l..r+1 {
+          result.count.push(Vec::new());
+          for _y in t..b+1 {
+            result.count.last_mut().unwrap().push(0);
+          }
         }
-        if y == BOARD_SIZE - 1 {
-          return true
+    }
+    for l in lines {
+      result.add(l);
+    }
+    result
+  }
+
+  fn increment(&mut self, p: &Point) {
+    match self.bounds {
+      Bounding::Empty => {},
+      Bounding::Box{l, r, t, b} =>
+        self.count[(p.x - l) as usize][(p.y - t) as usize] += 1
+    }
+  }
+  
+  fn add(&mut self, l: &Line) {
+    if l.is_horizontal() {
+      for x in l.left()..l.right()+1 {
+        self.increment(&Point{x: x, y: l.top()})
+      }
+    } else {
+      for y in l.top()..l.bottom()+1 {
+        self.increment(&Point{x: l.left(), y: y})
+      }
+    }
+  }
+
+  fn count<F>(&self, f: F) -> i64
+      where F: Fn(i32) -> bool {
+    let mut result: i64 = 0;
+    for col in &self.count {
+      for loc in col {
+        if f(*loc) {
+          result += 1;
         }
       }
     }
-
-    // look for winning columns
-    for y in 0..BOARD_SIZE {
-      for x in 0..BOARD_SIZE {
-        if !self.mark[x][y] {
-          break;
-        }
-        if x == BOARD_SIZE - 1 {
-          return true
-        }
-      }
-    }
-    false
+    result
   }
-
-  fn mark(&mut self, num: i32) {
-    for x in 0..BOARD_SIZE {
-      for y in 0..BOARD_SIZE {
-        if self.board[x][y] == num {
-          self.mark[x][y] = true;
-        }
-      }
-    }
-  }
-
-  fn score(&self, num: i32) -> i64 {
-    let mut sum : i64 = 0;
-    for x in 0..BOARD_SIZE {
-      for y in 0..BOARD_SIZE {
-        if !self.mark[x][y] {
-          sum += self.board[x][y] as i64;
-        }
-      }
-    }
-    sum * num as i64
-  }
-}
-
-fn read_board_list(stream: &mut dyn iter::Iterator<Item = String>)
-    -> Vec<BingoBoard> {
-  let mut peek = stream.peekable();
-  let mut result = Vec::new();
-  while peek.peek() != None {
-    result.push(read_board(peek.by_ref()));
-  }
-  result
-}
-
-fn read_board(stream: &mut dyn iter::Iterator<Item = String>) -> BingoBoard {
-  let mut result = BingoBoard::default();
-  for _ in 0..BOARD_SIZE {
-    let row: Vec<i32> = stream.next().unwrap().split_whitespace()
-       .map(|x| x.parse::<i32>().unwrap()).collect();
-    assert!(row.len() == BOARD_SIZE);
-    result.board.push(row);
-  }
-  assert!(result.board.len() == BOARD_SIZE);
-  result
-}
-
-fn run_moves(moves: &Vec<i32>, boards: &mut Vec<BingoBoard>) -> i64 {
-  for m in moves {
-    println!("Making move {}", *m);
-    for b in &mut *boards {
-      b.mark(*m);
-    }
-    if boards.len() == 1 && boards[0].won() {
-      return boards[0].score(*m)
-    }
-    boards.retain(|x| !x.won());
-  }
-  0
 }
