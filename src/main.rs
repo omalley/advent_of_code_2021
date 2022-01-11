3022cbd38502efeaaae6a27a5cacef06cfca1a52
Owen O'Malley <omalley@apache.org>
2022-01-10 19:42:19 -0800

    puzzle 7.2

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,22 @@ fn main() {
 
   // sort them and find median
   crabs.sort();
-  let median = crabs[crabs.len() / 2];
-  let cost = crabs.iter().fold(0, |cost, x| cost + (x - median).abs());
-  println!("location = {}, cost = {}", median, cost);  
+  let mut best_cost = i32::MAX;
+  let mut best = -1;
+  for g in crabs[0]..crabs[crabs.len() - 1] {
+    let new_cost = total_cost(&crabs, g);
+    if new_cost < best_cost {
+      best_cost = new_cost;
+      best = g;
+    }
+  }
+  println!("location = {}, cost = {}", best, best_cost);  
 }
 
+fn cost(n: i32) -> i32 {
+  (n + 1) * n / 2
+}
+
+fn total_cost(posns: &Vec<i32>, goal: i32) -> i32 {
+  posns.iter().fold(0, |total, x| total + cost((x - goal).abs()))
+}
