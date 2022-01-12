406649434acc9e7fd31f0dab862dd0c1cb996861
Owen O'Malley <omalley@apache.org>
2022-01-11 21:00:25 -0800

    puzzle 9.1

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,96 @@
-use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
 
 fn main() {
   let stdin = io::stdin();
-  let lines: Vec<Display> = stdin.lock().lines()
-     .map(|x| String::from(x.unwrap().trim()))
-     .filter(|x| x.len() > 0)
-     .map(|x| Display::parse(&x))
-     .collect();
-
-  let easy = lines.iter().map(|x| x.easy_numbers())
-    .fold(0, |acc, x| acc + x);
-  let sum = lines.iter().map(|x| x.unscramble())
-    .fold(0, |acc, x| acc + x);
-  println!("easy = {}, sum = {}", easy, sum);
+  let map = Map::parse(&mut stdin.lock().lines()
+                              .map(|x| String::from(x.unwrap().trim()))
+                              .filter(|x| x.len() > 0));
+  let mut risk = 0;
+  for x in 0..map.get_width() {
+    for y in 0..map.get_height() {
+      if map.is_low(x, y) {
+        risk += 1 + map.get_elevation(x, y)
+      }
+    }
+  }
+  println!("risk = {}", risk);
 }
 
 #[derive(Debug,Default)]
-struct Display {
-  digits: Vec<String>,
-  display: Vec<String>,
+struct Map {
+  elevation: Vec<Vec<u32>>,
+  width: usize,
 }
 
-impl Display {
-  fn parse(input: &str) -> Self {
-    let mut result = Display::default();
-    let parts: Vec<String> = input.split("|")
-        .map(|x| String::from(x)).collect();
-    result.digits = parts[0].split_whitespace().
-        map(|x| sort_word(x.trim())).collect();
-    result.digits.sort_by(|a, b| a.len().partial_cmp(&b.len()).unwrap());
-    result.display = parts[1].split_whitespace().
-        map(|x| sort_word(x.trim())).collect();
+const MAP_RADIX: u32 = 10;
+
+impl Map {
+  fn parse(input: &mut dyn Iterator<Item = String>) -> Self {
+    let mut result = Map::default();
+    result.elevation = input
+        .map(|x| x.chars().map(|c| c.to_digit(MAP_RADIX).unwrap()).collect())
+        .collect();
+    if result.elevation.len() != 0 {
+      result.width = result.elevation[0].len();
+    }
     result
   }
 
-  fn easy_numbers(&self) -> i32 {
-    let easy = vec![2, 3, 4, 7];
-    self.display.iter().filter(|x| easy.contains(&x.len())).count() as i32
+  fn get_width(&self) -> usize {
+    self.width
   }
 
-  fn unscramble(&self) -> i32 {
-    let mut trans = HashMap::new();
-    // map the easy ones
-    trans.insert(&self.digits[0], 1);
-    trans.insert(&self.digits[1], 7);
-    trans.insert(&self.digits[2], 4);
-    trans.insert(&self.digits[9], 8);
-
-    // now look at the 5 segment ones
-    for s in &self.digits[3..6] {
-      if overlap(s, &self.digits[0]) == 2 {
-        trans.insert(s, 3);
-      } else if overlap(s, &self.digits[2]) == 2 {
-        trans.insert(s, 2);
-      } else {
-        trans.insert(s, 5);
-      }
-    }
-
-    // now look at the 6 segment ones
-    for s in &self.digits[6..9] {
-      if overlap(s, &self.digits[2]) == 4 {
-        trans.insert(s, 9);
-      } else if overlap(s, &self.digits[0]) == 2 {
-        trans.insert(s, 0);
-      } else {
-        trans.insert(s, 6);
-      }
-    }
-    self.display.iter().fold(0, |acc, x| acc * 10 + trans.get(x).unwrap())
+  fn get_height(&self) -> usize {
+    self.elevation.len()
   }
-}
+  
+  fn get_elevation(&self, x: usize, y: usize) -> u32 {
+    self.elevation[y][x]
+  }
 
-fn sort_word(input: &str) -> String {
-  let mut chars: Vec<char> = input.chars().collect();
-  chars.sort();
-  String::from_iter(chars)
-}
+  fn get_left(&self, x: usize, y: usize) -> Option<u32> {
+    if x == 0 {
+      None
+    } else {
+      Some(self.get_elevation(x - 1, y))
+    }
+  }
+  
+  fn get_right(&self, x: usize, y: usize) -> Option<u32> {
+    if x == self.get_width() - 1 {
+      None
+    } else {
+      Some(self.get_elevation(x + 1, y))
+    }
+  }
+  
+  fn get_up(&self, x: usize, y: usize) -> Option<u32> {
+    if y == 0 {
+      None
+    } else {
+      Some(self.get_elevation(x, y - 1))
+    }
+  }
+  
+  fn get_down(&self, x: usize, y: usize) -> Option<u32> {
+    if y == self.get_height() - 1 {
+      None
+    } else {
+      Some(self.get_elevation(x, y + 1))
+    }
+  }
+  
+  fn is_low(&self, x: usize, y: usize) -> bool {
+    // get the elevations of the neighbors
+    let mut neighbors = Vec::new();
+    neighbors.extend(self.get_left(x, y).into_iter());
+    neighbors.extend(self.get_right(x, y).into_iter());
+    neighbors.extend(self.get_up(x, y).into_iter());
+    neighbors.extend(self.get_down(x, y).into_iter());
 
-fn overlap(long: &str, short: &str) -> i32 {
-  let goal: Vec<char> = short.chars().collect();
-  long.chars().filter(|c| goal.contains(c)).count() as i32
+    // get the minimum
+    let min = neighbors.iter().fold(u32::MAX, |acc, x| u32::min(acc, *x));
+    self.get_elevation(x, y) < min
+  }
 }
