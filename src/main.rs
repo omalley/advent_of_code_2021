fd12c008bd1601d65f5d173c2920679c18416088
Owen O'Malley <omalley@apache.org>
2022-01-15 23:10:55 -0800

    puzzle 12.1

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,110 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
+use std::rc::Rc;
 
 fn main() {
   let stdin = io::stdin();
-  let mut octo = Octopus::parse(&mut stdin.lock().lines()
-                                 .map(|x| String::from(x.unwrap().trim()))
-                                 .filter(|x| x.len() > 0));
+  let caves = Rc::new(CaveSystem::parse(&mut stdin.lock().lines()
+                        .map(|x| String::from(x.unwrap().trim()))
+                        .filter(|x| x.len() > 0)));
 
-  loop {
-    if octo.advance() == octo.width * octo.energy.len() {
-      break;
-    }
-  }
-  println!("turns = {}", octo.turn);
+  println!("paths = {}", PathState::new(caves.clone()).count());
 }
 
-#[derive(Debug,Default)]
-struct Octopus {
-  energy: Vec<Vec<u32>>,
-  width: usize,
-  turn: u64,
+#[derive(Default,Debug)]
+struct CaveSystem {
+  caves: HashMap<String,Cave>,
 }
 
-#[derive(Clone,Copy,Debug)]
-struct Point {
-  x: usize,
-  y: usize,
+#[derive(Default,Debug)]
+struct Cave {
+  passages: Vec<String>,
+  is_big: bool,
+  is_end: bool,
 }
 
-const OCTOPUS_RADIX: u32 = 10;
-
-impl Octopus {
-  fn parse(input: &mut dyn Iterator<Item = String>) -> Self {
-    let mut result = Octopus::default();
-    result.energy = input.map(|line| line.chars()
-                                .map(|c| c.to_digit(OCTOPUS_RADIX).unwrap())
-                                .collect())
-                         .collect();
-    result.width =
-      match result.energy.iter().map(|x| x.len())
-              .reduce(|a, b| usize::min(a, b)) {
-        None => 0,
-        Some(x) => x,
-      };
-    result
-  }
-
-  fn neighbors(&self, pnt: &Point) -> Vec<Point> {
-    let mut result: Vec<Point> = Vec::new();
-    for relative_x in -1..=1 {
-      for relative_y in -1..=1 {
-        if relative_x != 0 || relative_y != 0 {
-          let off_x = pnt.x as i64 + relative_x;
-          let off_y = pnt.y as i64 + relative_y;
-          if off_x >= 0 && off_x < self.width as i64 &&
-             off_y >= 0 && off_y < self.energy.len() as i64 {
-            result.push(Point{x: off_x as usize, y: off_y as usize});
-          }
-        }
-      }
-    }
-    result
-  }
+impl CaveSystem {
+  const START: &'static str = "start";
+  const END: &'static str = "end";
   
-  fn advance(&mut self) -> usize {
-    let mut to_do: Vec<Point> = Vec::new();
-    for x in 0..self.width {
-      for y in 0..self.energy.len() {
-        to_do.push(Point{x : x, y: y});
-      }
+  fn parse(input: &mut dyn Iterator<Item = String>) -> Self {
+    let mut result = CaveSystem::default();
+    for line in input {
+      let parts: Vec<String> = line.split("-")
+          .map(|x| String::from(x.trim())).collect();
+      assert!(parts.len() == 2);
+      result.create_cave(&parts[0], &parts[1]);
+      result.create_cave(&parts[1], &parts[0]);
     }
+    result
+  }
 
-    // update all of the squares
-    while to_do.len() > 0 {
-      let p = to_do.pop().unwrap();
-      self.energy[p.y][p.x] += 1;
-      // if it went to 10, bump up the neighbors again
-      if self.energy[p.y][p.x] == OCTOPUS_RADIX {
-        to_do.extend(self.neighbors(&p).iter());
-      }
+  fn create_cave(&mut self, name: &str, dest: &str)  {
+    if !self.caves.contains_key(name) {
+      let mut new_cave = Cave::default();
+      new_cave.is_big = name.chars().next().unwrap().is_ascii_uppercase();
+      new_cave.is_end = name == CaveSystem::END;
+      self.caves.insert(String::from(name), new_cave);
     }
+    if name != CaveSystem::END {
+      self.caves.get_mut(name).unwrap().passages.push(String::from(dest));
+    }
+  }
+}
 
-    self.turn += 1;
-    
-    let mut lights = 0;
-    for x in 0..self.width {
-      for y in 0..self.energy.len() {
-        if self.energy[y][x] >= OCTOPUS_RADIX {
-          self.energy[y][x] = 0;
-          lights += 1;
+#[derive(Debug)]
+struct Decision {
+  name: String,
+  next: usize,
+}
+
+impl Decision {
+  fn new(name: &str) -> Self {
+    Decision{name: String::from(name), next: 0}
+  }
+}
+
+#[derive(Debug)]
+struct PathState {
+  caves: Rc<CaveSystem>,
+  path: Vec<Decision>,
+}
+
+impl PathState {
+  fn new(caves: Rc<CaveSystem>) -> Self {
+    PathState{path: vec![Decision::new(CaveSystem::START)],
+              caves: caves.clone()}
+  }
+
+  fn can_advance(&self, next: &str, is_big: bool) -> bool {
+    is_big || !self.path.iter().any(|x| x.name == next)
+  }
+}
+
+impl Iterator for PathState {
+  type Item = Vec<String>;
+  
+  fn next(&mut self) -> Option<Self::Item> {
+    while self.path.len() > 0 {
+      let last_entry: usize = self.path.len() - 1;
+      let mut current = &mut self.path[last_entry];
+      let current_cave = &self.caves.caves[&current.name];
+      if current.next >= current_cave.passages.len() {
+        self.path.pop();
+      } else {
+        let next = &current_cave.passages[current.next];
+        current.next += 1;
+        if self.can_advance(next, self.caves.caves[next].is_big) {
+          self.path.push(Decision::new(next));
+        }
+        if next == CaveSystem::END {
+          return Some(self.path.iter()
+                          .map(|x| x.name.clone())
+                          .collect())
         }
       }
     }
-    lights
+    None
   }
 }
