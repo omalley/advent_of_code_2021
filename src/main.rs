9abc5a57d3e4edcfee4224f26b3f44d5238be146
Owen O'Malley <omalley@apache.org>
2022-01-16 22:52:41 -0800

    puzzle 14.1

--- src/main.rs
+++ src/main.rs
@@ -1,119 +1,63 @@
+use std::collections::HashMap;
 use std::io;
 use std::io::BufRead;
-
-use lazy_static::lazy_static;
-use regex::Captures;
-use regex::Regex;
 
 fn main() {
   let stdin = io::stdin();
   let mut problem = Problem::parse(&mut stdin.lock().lines()
                                      .map(|x| String::from(x.unwrap().trim()))
                                      .filter(|x| x.len() > 0));
-  for f in 0..problem.folds.len() {
-    problem.do_fold(f);
-    println!("count = {}", problem.count());
+  const ITERATIONS: i64 = 10;
+  for i in 0..ITERATIONS {
+    println!("iteration {}", i);
+    problem.grow();
   }
-  problem.draw();
+  println!("iter {} = {}", ITERATIONS, problem.score());
 }
 
-#[derive(Debug)]
-enum Fold {
-  Horizontal {y : usize},
-  Vertical {x: usize}
-}
-
-#[derive(Clone,Copy,Debug,Eq,Ord,PartialEq,PartialOrd)]
-struct Point {
-  x: usize,
-  y: usize,
-}
-
-#[derive(Default,Debug)]
+#[derive(Debug, Default)]
 struct Problem {
-  points: Vec<Point>,
-  folds: Vec<Fold>,
+  current: String,
+  insertions: HashMap<String, String>,
 }
 
 impl Problem {
-  fn get_number(cap: &Captures, name: &str) -> usize {
-    cap.name(name).unwrap().as_str().parse::<usize>().unwrap()
-  }
-  
   fn parse(input: &mut dyn Iterator<Item = String>) -> Self {
-    lazy_static! {
-      static ref POINT_RE: Regex
-        = Regex::new(r"^(?P<x>\d+),(?P<y>\d+)$").unwrap();
-      static ref FOLD_RE: Regex
-        = Regex::new(r"^fold\s+along\s+(?P<dir>[xy])=(?P<val>\d+)$").unwrap();
-    }
     let mut result = Problem::default();
+    result.current = input.next().unwrap();
     for line in input {
-      match FOLD_RE.captures(&line) {
-        None =>
-          match POINT_RE.captures(&line) {
-            None => panic!("Bad point {}", line),
-            Some(cap) =>
-              result.points.push(Point{
-                x: Problem::get_number(&cap, "x"),
-                y: Problem::get_number(&cap, "y")
-              }),
-          }
-        Some(cap) =>
-          match cap.name("dir").unwrap().as_str() {
-            "x" => result.folds.push(Fold::Vertical{
-              x: Problem::get_number(&cap, "val")}),
-            "y" => result.folds.push(Fold::Horizontal{
-              y: Problem::get_number(&cap, "val")}),
-            _ => panic!("Bad fold {}", line),
-          }
-      }
+      let parts: Vec<&str> = line.split("->").map(|s| s.trim()).collect();
+      result.insertions.insert(String::from(parts[0]), String::from(parts[1]));
     }
     result
   }
 
-  fn do_fold(&mut self, fold_idx: usize) {
-    match self.folds.get(fold_idx).unwrap() {
-      Fold::Vertical{x: vf} =>
-        self.points =
-          self.points.iter().map(|p|
-            if p.x > *vf {
-              Point{x: 2* vf - p.x, y: p.y}
-            } else {
-              *p
-            }).collect(),
-      Fold::Horizontal{y: hf} =>
-        self.points =
-          self.points.iter().map(|p|
-            if p.y > *hf {
-              Point{x: p.x, y: 2 * hf - p.y}
-            } else {
-              *p
-            }).collect(),
-    }
-    self.points.sort_unstable();
-    self.points.dedup();
-  }
-
-  fn count(&self) -> usize {
-    self.points.len()
-  }
-
-  fn draw(&self) {
-    let max_x = self.points.iter()
-        .map(|p| p.x).fold(0, |a,b| usize::max(a, b));
-    let max_y = self.points.iter()
-        .map(|p| p.y).fold(0, |a,b| usize::max(a, b));
-    let mut result: Vec<Vec<bool>> =
-      vec![vec![false; max_x + 1]; max_y + 1];
-    for p in &self.points {
-      result[p.y][p.x] = true;
-    }
-    for line in result {
-      for posn in line {
-        print!("{}", if posn {'#'} else {' '})
+  fn grow(&mut self) {
+    if self.current.len() > 1 {
+      let mut chars = self.current.chars();
+      let mut result = String::new();
+      let mut prev = chars.next().unwrap();
+      result.push(prev);
+      for cur in chars {
+        let mut key = String::from(prev);
+        key.push(cur);
+        if self.insertions.contains_key(&key) {
+          result.push_str(self.insertions.get(&key).unwrap());
+        }
+        result.push(cur);
+        prev = cur;
       }
-      println!("")
+      self.current = result;
     }
+  }
+
+  fn score(&self) -> u64 {
+    let mut cnt: HashMap<char, u64> = HashMap::new();
+    for ch in self.current.chars() {
+      cnt.insert(ch, cnt.get(&ch).unwrap_or(&0) + 1);
+    }
+    let mut sum: Vec<u64> = cnt.iter().map(|(_c,v)| *v).collect();
+    sum.sort();
+    sum.get(sum.len() - 1).unwrap() - sum.get(0).unwrap()
   }
 }
