8bd1236026643b9f7621f647c34554328862b96a
Owen O'Malley <omalley@apache.org>
2022-01-17 16:52:14 -0800

    puzzle 16.1

--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,92 @@
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 use std::io;
 use std::io::BufRead;
 
+use bitreader::BitReader;
+
 fn main() {
   let stdin = io::stdin();
-  let problem = Problem::parse(&mut stdin.lock().lines()
-                                 .map(|x| String::from(x.unwrap().trim()))
-                                 .filter(|x| x.len() > 0), 5);
-//  println!("board = {:?}", problem);                                 
-  println!("lowest = {}", problem.find_lowest());
-}
-
-#[derive(Debug, Default)]
-struct Problem {
-  risk: Vec<Vec<u32>>,
-  width: usize,
-}
-
-#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Point {
-  x: usize,
-  y: usize,
-}
-
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-struct ToDoItem {
-  cost: u32,
-  position: Point,
-}
-
-impl Ord for ToDoItem {
-  fn cmp(&self, other: &Self) -> Ordering {
-    other.cost.cmp(&self.cost)
-         .then_with(|| self.position.cmp(&other.position))
+  for line in stdin.lock().lines()
+                   .map(|x| String::from(x.unwrap().trim()))
+                   .filter(|x| x.len() > 0) {
+    let pack = Packet::parse(&line);
+    println!("pack = {:?}, version = {}", pack, pack.version_sum());
   }
 }
 
-impl PartialOrd for ToDoItem {
-  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-    Some(self.cmp(other))
-  }
+#[derive(Debug)]
+struct Packet {
+  version: u8,
+  kind: PacketKind,
 }
 
-impl Problem {
-  const RISK_RADIX: u32 = 10;
+#[derive(Debug)]
+enum PacketKind {
+  Literal(u64),
+  Operator{op: u8, children: Vec<Packet>},
+}
+
+impl Packet {
+  fn parse(input: &str) -> Self {
+    let vec: Vec<u8> = hex::decode(input).unwrap();
+    let mut reader = BitReader::new(&vec);
+    Packet::parse_packet(&mut reader)
+  }
+
+  fn parse_packet(reader: &mut BitReader) -> Self {
+    let version = reader.read_u8(3).unwrap();
+    let kind_code = reader.read_u8(3).unwrap();
+    let kind = match kind_code {
+      4 => PacketKind::Literal(Packet::parse_literal(reader)),
+      _ => PacketKind::Operator{op: kind_code,
+                                children: Packet::parse_children(reader)},
+    };
+    Packet{version: version, kind: kind}
+  }
+
+  const LITERAL_CONT_MASK: u64 = 0x10;
   
-  fn parse(input: &mut dyn Iterator<Item = String>,
-           multiplier: u32) -> Self {
-    let mut result = Problem::default();
-    // read the template in
-    let template: Vec<Vec<u32>> = input.map(|line| line.chars()
-       .map(|c| c.to_digit(Problem::RISK_RADIX).unwrap()).collect())
-       .collect();
-    for tile_y in 0..multiplier {
-      for template_row in &template {
-        let mut row: Vec<u32> = Vec::new();
-        for tile_x in 0..multiplier {
-          for val in template_row {
-            row.push((val + tile_y + tile_x - 1) %
-                        (Problem::RISK_RADIX - 1) + 1);
-          }
-        }
-        result.risk.push(row);
+  fn parse_literal(reader: &mut BitReader) -> u64 {
+    let mut result: u64 = 0;
+    let mut chunks: u64 = 0;
+    loop {
+      let chunk = reader.read_u64(5).unwrap();
+      result = (result << 4) | (chunk & !Packet::LITERAL_CONT_MASK);
+      if chunk & Packet::LITERAL_CONT_MASK == 0 {
+        break
+      }
+      chunks += 1;
+      if chunks >= 16 {
+        panic!("overflow in reading literal");
       }
     }
-    result.width =
-      match result.risk.iter().map(|x| x.len())
-              .reduce(|a, b| usize::min(a, b)) {
-        None => 0,
-        Some(x) => x,
-      };
     result
   }
 
-  fn find_neighbors(&self, point: &Point) -> Vec<Point> {
-    let mut result: Vec<Point> = Vec::new();
-    if point.x > 0 {
-      result.push(Point{x: point.x - 1, y: point.y});
-    }
-    if point.y > 0 {
-      result.push(Point{x: point.x, y: point.y - 1});
-    }
-    if point.y < self.risk.len() - 1 {
-      result.push(Point{x: point.x, y: point.y + 1});
-    }
-    if point.x < self.width - 1 {
-      result.push(Point{x: point.x + 1, y: point.y});
+  fn parse_children(reader: &mut BitReader) -> Vec<Packet> {
+    let mut result: Vec<Packet> = Vec::new();
+    let length_type = reader.read_u8(1).unwrap();
+    if length_type == 0 {
+      let len = reader.read_u64(15).unwrap();
+      let limit = reader.position() + len;
+      while reader.position() < limit {
+        result.push(Packet::parse_packet(reader));
+      }
+    } else {
+      let kids = reader.read_u64(11).unwrap();
+      for _i in 0..kids {
+        result.push(Packet::parse_packet(reader));
+      }
     }
     result
   }
-  
-  fn find_lowest(&self) -> u32 {
-    let mut best: Vec<Vec<u32>> =
-        vec![vec![ u32::MAX; self.width]; self.risk.len()];
-    best[0][0] = 0;
-    let mut to_do: BinaryHeap<ToDoItem> = BinaryHeap::new();
-    to_do.push(ToDoItem{cost:0, position: Point{x:0, y:0}});
-    while let Some(ToDoItem{cost: _, position}) = to_do.pop() {
-      for neighbor in &self.find_neighbors(&position) {
-        let new_risk =
-            self.risk[neighbor.y][neighbor.x] + best[position.y][position.x];
-        if new_risk < best[neighbor.y][neighbor.x] {
-          best[neighbor.y][neighbor.x] = new_risk;
-          to_do.push(ToDoItem{cost: new_risk, position: neighbor.clone()});
-        }
+
+  fn version_sum(&self) -> u64 {
+    self.version as u64 +
+      match &self.kind {
+        PacketKind::Literal(_) => 0,
+        PacketKind::Operator{op: _, children} =>
+          children.iter().map(|c| c.version_sum())
+                         .fold(0, |a, b| a + b),
       }
-    }
-    best[self.risk.len() -1][self.width - 1]
   }
 }
