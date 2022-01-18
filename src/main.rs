3930ac7d43bebb40cd68667f85bd5e91f51144d1
Owen O'Malley <omalley@apache.org>
2022-01-17 20:15:38 -0800

    puzzle 16.2

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,8 @@ fn main() {
                    .map(|x| String::from(x.unwrap().trim()))
                    .filter(|x| x.len() > 0) {
     let pack = Packet::parse(&line);
-    println!("pack = {:?}, version = {}", pack, pack.version_sum());
+    println!("pack = {:?}, version = {}, eval = {}",
+      pack, pack.version_sum(), pack.evaluation());
   }
 }
 
@@ -21,8 +22,14 @@ struct Packet {
 
 #[derive(Debug)]
 enum PacketKind {
+  Sum(Vec<Packet>),
+  Product(Vec<Packet>),
+  Minimum(Vec<Packet>),
+  Maximum(Vec<Packet>),
   Literal(u64),
-  Operator{op: u8, children: Vec<Packet>},
+  Greater(Vec<Packet>),
+  Less(Vec<Packet>),
+  Equal(Vec<Packet>),
 }
 
 impl Packet {
@@ -36,9 +43,15 @@ impl Packet {
     let version = reader.read_u8(3).unwrap();
     let kind_code = reader.read_u8(3).unwrap();
     let kind = match kind_code {
+      0 => PacketKind::Sum(Packet::parse_children(reader)),
+      1 => PacketKind::Product(Packet::parse_children(reader)),
+      2 => PacketKind::Minimum(Packet::parse_children(reader)),
+      3 => PacketKind::Maximum(Packet::parse_children(reader)),
       4 => PacketKind::Literal(Packet::parse_literal(reader)),
-      _ => PacketKind::Operator{op: kind_code,
-                                children: Packet::parse_children(reader)},
+      5 => PacketKind::Greater(Packet::parse_children(reader)),
+      6 => PacketKind::Less(Packet::parse_children(reader)),
+      7 => PacketKind::Equal(Packet::parse_children(reader)),
+      _ => panic!("bad kind {}", kind_code),
     };
     Packet{version: version, kind: kind}
   }
@@ -80,13 +93,47 @@ impl Packet {
     result
   }
 
+  fn children_sum(children: &Vec<Packet>) -> u64 {
+     children.iter().map(|c| c.version_sum())
+                    .fold(0, |a, b| a + b)
+  }
+
   fn version_sum(&self) -> u64 {
     self.version as u64 +
       match &self.kind {
         PacketKind::Literal(_) => 0,
-        PacketKind::Operator{op: _, children} =>
-          children.iter().map(|c| c.version_sum())
-                         .fold(0, |a, b| a + b),
+        PacketKind::Sum(kids) => Packet::children_sum(kids),
+        PacketKind::Product(kids) => Packet::children_sum(kids),
+        PacketKind::Minimum(kids) => Packet::children_sum(kids),
+        PacketKind::Maximum(kids) => Packet::children_sum(kids),
+        PacketKind::Greater(kids) => Packet::children_sum(kids),
+        PacketKind::Less(kids) => Packet::children_sum(kids),
+        PacketKind::Equal(kids) => Packet::children_sum(kids),
       }
+  }
+
+  fn evaluation(&self) -> u64 {
+    match &self.kind {
+      PacketKind::Literal(lit) => *lit,
+      PacketKind::Sum(kids) =>
+        kids.iter().map(|k| k.evaluation()).fold(0, |a, b| a + b),
+      PacketKind::Product(kids) =>
+        kids.iter().map(|k| k.evaluation()).fold(1, |a, b| a * b),
+      PacketKind::Minimum(kids) =>
+        kids.iter().map(|k| k.evaluation())
+                   .reduce(|a, b| u64::min(a, b)).unwrap(),
+      PacketKind::Maximum(kids) =>
+        kids.iter().map(|k| k.evaluation())
+                   .reduce(|a, b| u64::max(a, b)).unwrap(),
+      PacketKind::Greater(kids) =>
+        kids.iter().map(|k| k.evaluation())
+                   .reduce(|a, b| if a > b { 1 } else { 0 }).unwrap(),
+      PacketKind::Less(kids) =>
+        kids.iter().map(|k| k.evaluation())
+                   .reduce(|a, b| if a < b { 1 } else { 0 }).unwrap(),
+      PacketKind::Equal(kids) =>
+        kids.iter().map(|k| k.evaluation())
+                   .reduce(|a, b| if a == b { 1 } else { 0 }).unwrap(),
+    }
   }
 }
