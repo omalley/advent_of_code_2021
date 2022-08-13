6004746eddd395ffa5602bf72cc4810bf7fcab44
Owen O'Malley <omalley@apache.org>
2022-08-13 15:24:34 -0700

    rearranging

--- src/main.rs
+++ src/main.rs
@@ -188,9 +188,7 @@ impl InputDescriptor {
 
 impl Display for InputDescriptor {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-    let mut nums = self.inputs.clone();
-    nums.sort();
-    let result = nums.iter()
+    let result = self.inputs.iter()
       .map(|s| InputDescriptor::num_to_string(*s))
       .collect::<Vec<String>>().join(", ");
     write!(f, "{}", result)
@@ -199,12 +197,63 @@ impl Display for InputDescriptor {
 
 #[derive(Clone, Debug)]
 struct SymbolicState {
-  registers: [HashMap<i64, InputDescriptor>; Register::SIZE],
+  register: [HashMap<i64, InputDescriptor>; Register::SIZE],
+}
+
+impl SymbolicState {
+  /// Generate all possible values for an input statement
+  fn do_input(idx: usize) -> HashMap<i64, InputDescriptor> {
+    (1 .. 10).map(|v| (v, InputDescriptor::init(idx, v))).collect()
+  }
+
+  fn do_add(&self, reg: &Register, opd: &Operand) -> HashMap<i64, InputDescriptor> {
+    HashMap::new()
+  }
+
+  fn do_multiply(&self, reg: &Register, opd: &Operand) -> HashMap<i64, InputDescriptor> {
+    HashMap::new()
+  }
+  fn do_divide(&self, reg: &Register, opd: &Operand) -> HashMap<i64, InputDescriptor> {
+    HashMap::new()
+  }
+  fn do_modulo(&self, reg: &Register, opd: &Operand) -> HashMap<i64, InputDescriptor> {
+    HashMap::new()
+  }
+  fn do_equals(&self, reg: &Register, opd: &Operand) -> HashMap<i64, InputDescriptor> {
+    HashMap::new()
+  }
+
+  // Evaluate the operation in the given state and input.
+  // Updates the state.
+  fn evaluate(&mut self, program: &[Operation], input: &[i64]) {
+    if program.len() > 0 {
+      match &program[0] {
+        Operation::Input(id, reg) =>
+          self.register[reg.index()] = Self::do_input(*id),
+        Operation::Add(reg, operand) =>
+          self.register[reg.index()] = self.do_add(reg, operand),
+        Operation::Multiply(reg, operand) =>
+          self.register[reg.index()] = self.do_multiply(reg, operand),
+        Operation::Divide(reg, operand) =>
+          self.register[reg.index()] = self.do_divide(reg, operand),
+        Operation::Modulo(reg, operand) =>
+          self.register[reg.index()] = self.do_modulo(reg, operand),
+        Operation::Equal(reg, operand) =>
+          self.register[reg.index()] = self.do_equals(reg, operand),
+      }
+      self.evaluate(&program[1..], input)
+    }
+  }
 }
 
 fn main() {
-  let program = Operation::parse(&mut io::stdin().lock().lines()).unwrap();
-  let input = vec![3,9,9,9,9,6,9,8,7,9,9,4,2,9];
+  let program = Operation::parse_file("little.txt").unwrap();
+  let stdin = io::stdin();
+  let input: Vec<i64> = stdin.lock().lines()
+          .map(|x| String::from(x.unwrap()))
+          .filter(|x| x.len() > 0)
+          .map(|x| x.parse::<i64>().unwrap())
+          .collect();
   let mut state = State::default();
   state.evaluate(&program, &input);
   println!("result = {:?}", &state);
